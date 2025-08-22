f946a8ce4f3b40871bf72979eb8b150609a6b9c2
Djbarrios26 <djbarrios26@gmail.com>
2025-08-21 23:26:56 -0500

    Data
    Conexion data base and Add files Csv

---
+++ server/conexion_db.js
@@ -0,0 +1,25 @@
+import pkg from "pg";
+const { Pool } = pkg;
+
+export const pool = new Pool({
+    host:       "localhost",
+    database:   "Learnex",         // Database name in PostgreSQL
+    port:       5432,              // PostgreSQL default port
+    user:       "postgres",        // PostgreSQL user
+    password:   "djbarrios26",     // Password
+    max:        10,                // maximum active connections
+    idleTimeoutMillis: 30000,      // maximum downtime before closing
+    connectionTimeoutMillis: 2000  // connection wait time
+});
+
+async function DataBaseConnection() {
+    try {
+        const client = await pool.connect();
+        console.log("✅ Connection to PostgreSQL successful");
+        client.release(); // liberar conexión al pool
+    } catch (error) {
+        console.error("❌ Error connecting to PostgreSQL:", error.message);
+    }
+}
+
+DataBaseConnection();
