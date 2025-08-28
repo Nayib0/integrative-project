d8fe4bd7e3c333802d0dd771d78ca2804e5f3597
Djbarrios26 <djbarrios26@gmail.com>
2025-08-28 00:09:23 -0500

    Create Tables
    Script for de create tables

--- docs/script.sql
+++ docs/script.sql
@@ -6,7 +6,7 @@ CREATE DATABASE learnex;
 CREATE SCHEMA learnex
 
 -- USERS
-CREATE TABLE users (
+CREATE TABLE learnex.users (
     id_user INT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     last_name VARCHAR(100) NOT NULL,
@@ -17,37 +17,37 @@ CREATE TABLE users (
 );
 
 -- CURSES
-CREATE TABLE curses (
+CREATE TABLE learnex.curses (
     id_curse INT PRIMARY KEY,
     grade VARCHAR(20) NOT NULL,
     school_year INT NOT NULL
 );
 
 -- SUBJECTS
-CREATE TABLE subjects (
+CREATE TABLE learnex.subjects (
     id_subjects INT PRIMARY KEY,
     name_subject VARCHAR(100) NOT NULL
 );
 
 -- CURSE - SUBJECT - TEACHER (RELATION)
-CREATE TABLE curse_subject_teacher (
+CREATE TABLE learnex.curse_subject_teacher (
     id_cst INT PRIMARY KEY,
-    id_curse INT NOT NULL REFERENCES curses(id_curse) ON DELETE CASCADE,
-    id_subject INT NOT NULL REFERENCES subjects(id_subjects) ON DELETE CASCADE,
-    id_teacher INT NOT NULL REFERENCES users(id_user) ON DELETE CASCADE
+    id_curse INT NOT NULL REFERENCES learnex.curses(id_curse) ON DELETE CASCADE,
+    id_subject INT NOT NULL REFERENCES learnex.subjects(id_subjects) ON DELETE CASCADE,
+    id_teacher INT NOT NULL REFERENCES learnex.users(id_user) ON DELETE CASCADE
 );
 
 -- NOTES
-CREATE TABLE notes (
+CREATE TABLE learnex.notes (
     id_note INT PRIMARY KEY,
-    id_student INT NOT NULL REFERENCES users(id_user) ON DELETE CASCADE,
-    id_cst INT NOT NULL REFERENCES curse_subject_teacher(id_cst) ON DELETE CASCADE,
+    id_student INT NOT NULL REFERENCES learnex.users(id_user) ON DELETE CASCADE,
+    id_cst INT NOT NULL REFERENCES learnex.curse_subject_teacher(id_cst) ON DELETE CASCADE,
     calification NUMERIC(3,1) NOT NULL
 );
 
 -- STUDENTS - CURSES (RELATION)
-CREATE TABLE students_curses (
+CREATE TABLE learnex.students_curses (
     id_student_curse INT PRIMARY KEY,
-    id_user INT NOT NULL REFERENCES users(id_user) ON DELETE CASCADE,
-    id_curse INT NOT NULL REFERENCES curses(id_curse) ON DELETE CASCADE
+    id_user INT NOT NULL REFERENCES learnex.users(id_user) ON DELETE CASCADE,
+    id_curse INT NOT NULL REFERENCES learnex.curses(id_curse) ON DELETE CASCADE
 );
